bf94850e81e6d9eff119e39a8a922bab65778896
Charlie Cano Muñoz <c.canom.2021@alumnos.urjc.es>
2022-04-24 23:57:16 +0200

    Alert is being shown in every email

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const fs = require('fs');
-const path = require('path');
+const { extractURLs } = require('ioc-extractor');
 
 // loader-code: wait until gmailjs has finished loading, before triggering actual extensiode-code.
 const loaderId = setInterval(() => {
@@ -13,33 +12,39 @@ const loaderId = setInterval(() => {
     startExtension(window._gmailjs);
 }, 100);
 
+function everythingOk(){
+    console.log("PARECE QUE NO HAY PHISHING");
+
+    var detector = document.createElement("div");
+    detector.innerHTML = "PARECE QUE TODO OK TRANKI";
+    detector.id = "phishing-detector-ok";
+
+    var parentDiv = document.querySelector("div.AO").parentNode;
+    var currentDiv = document.querySelector("div.AO");
+    parentDiv.insertBefore(detector, currentDiv);
+}
+
+function possiblePhishing(){
+    console.log("CUIDADO QUE PARECE QUE PUEDE HABER PHISHING");
+
+    var detector = document.createElement("div");
+    detector.innerHTML = "PARECE QUE TE JUANKEAN CUIDAO";
+    detector.id = "phishing-detector-ko";
+
+    var parentDiv = document.querySelector("div.AO").parentNode;
+    var currentDiv = document.querySelector("div.AO");
+    parentDiv.insertBefore(detector, currentDiv);
+}
+
 // actual extension-code
 function startExtension(gmail) {
     console.log("¡Bienvenido a Phishing Detector!");
     window.gmail = gmail;
 
-    // As with JSON, use the Fetch API & ES6
-/*fetch('./data/phishtank.json')
-  .then(response => response.text())
-  .then(dataPhishtank => {
-      console.log(dataPhishtank);
-  	phishtank = JSON.parse(dataPhishtank);
-    urls = phishtank.map(r = r.url);
-  });*/
-
   var phishtankData = require('./data/phishtank.json');
-  //var phishtankJson = JSON.parse(phishtankData);
   var urls = phishtankData.map(r => r.url);
-
-    
-    /*let url = 'https://data.phishtank.com/data/online-valid.json';
-
-    fetch(url, {mode: 'no-cors', headers: {"Accept": "application/json", "Content-Type": "application/json", "User-Agent": "Phishtank/Charlie"}})
-    .then(res => res.json())
-    .then(out => console.log(out))
-    .catch(err => console.log(err));*/
-
-    //let urls = phishtank.map(r => r.url);
+  urls.push("http://magiaycardistry.activehosted.com/proc.php?nl=2&amp;c=294&amp;m=11742&amp;s=bb4584f418ac004322ff7e0746b46a63&amp;act=unsub");
+  let isPhishing = false;
 
     gmail.observe.on("load", () => {
         const userEmail = gmail.get.user_email();
@@ -49,12 +54,28 @@ function startExtension(gmail) {
         gmail.observe.on("view_email", (domEmail) => {
             console.log("Entrando en el email:", domEmail);
             const emailData = gmail.new.get.email_data(domEmail);
-            console.log("Todos los datos del email:", emailData);
+            //console.log("Todos los datos del email:", emailData);
             console.log("Asunto del mensaje: ", emailData.subject);
             console.log("Cuerpo del mensaje: ", emailData.content_html);
             console.log("Remitente del mensaje (Nombre): ", emailData.from.name);
             console.log("Remitente del mensaje (Email): ", emailData.from.address);
             console.log("lista de destinatarios: ", emailData.to);
+
+            let emailUrls = extractURLs(emailData.content_html);
+            console.log("URLS DEL EMAIL", emailUrls);
+
+            emailUrls.every(url => {
+                if(urls.includes(url)){
+                    isPhishing = true;
+                    return false;
+                }
+            })
+
+            if(isPhishing){
+                possiblePhishing();
+            } else {
+                everythingOk();
+            }
         });
 
         gmail.observe.on("compose", (compose) => {
