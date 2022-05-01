452d276a320253384b6bf593397b4db970c07255
Charlie Cano Muñoz <c.canom.2021@alumnos.urjc.es>
2022-05-01 13:36:04 +0200

    Data is retrieved from Firebase

--- src/extension.js
+++ src/extension.js
@@ -1,6 +1,26 @@
 "use strict";
+/* eslint-disable */
 
+//const { initializeApp } = require('firebase/app');
 const { extractURLs } = require('ioc-extractor');
+
+var firebase = require('firebase');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCZlFeGtKjRDqhc6nmzlZ_VNZSTlQR3RKo",
+    authDomain: "tfm-phishing-iocs.firebaseapp.com",
+    databaseURL: "https://tfm-phishing-iocs-default-rtdb.firebaseio.com",
+    projectId: "tfm-phishing-iocs",
+    storageBucket: "tfm-phishing-iocs.appspot.com",
+    messagingSenderId: "679351493087",
+    appId: "1:679351493087:web:e0fd9ba1fe6a5e2cfe646a"
+  };
+
+  // Initialize Firebase
+var app = firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+//const database = getDatabase(app);
 
 // loader-code: wait until gmailjs has finished loading, before triggering actual extensiode-code.
 const loaderId = setInterval(() => {
@@ -36,15 +56,24 @@ function possiblePhishing(){
     parentDiv.insertBefore(detector, currentDiv);
 }
 
+function deleteElements(){
+    var ko = document.getElementById("phishing-detector-ko");
+    var ok = document.getElementById("phishing-detector-ok");
+
+    if(ko !== null) {ko.remove();}
+    if(ok !== null) {ok.remove();}
+    
+}
+
 // actual extension-code
 function startExtension(gmail) {
     console.log("¡Bienvenido a Phishing Detector!");
     window.gmail = gmail;
 
-  var phishtankData = require('./data/phishtank.json');
-  var urls = phishtankData.map(r => r.url);
-  urls.push("http://magiaycardistry.activehosted.com/proc.php?nl=2&amp;c=294&amp;m=11742&amp;s=bb4584f418ac004322ff7e0746b46a63&amp;act=unsub");
-  let isPhishing = false;
+    var phishtankData = require('./data/phishtank.json');
+    var urls = phishtankData.map(r => r.url);
+    urls.push("https://ci3.googleusercontent.com/proxy/HErSrFtf2BEIMYi6manLl9y1DJRtAI0RKk09fcf1eqpN0ninTDclOExzf0JshaxeC3HBZ_eKpCkXcJ5lQQ39GtrRSOoEYAD-Rojt2IyszVcT=s0-d-e1-ft#http://mi.udemy.com/p/rp/89123c39ab29a5f1.png?mi_u=31671973825");
+
 
     gmail.observe.on("load", () => {
         const userEmail = gmail.get.user_email();
@@ -60,6 +89,13 @@ function startExtension(gmail) {
             console.log("Remitente del mensaje (Nombre): ", emailData.from.name);
             console.log("Remitente del mensaje (Email): ", emailData.from.address);
             console.log("lista de destinatarios: ", emailData.to);
+
+            firebase.database().ref('/').once('value', snapshot => {
+                console.log(snapshot.val());
+              });
+
+            let isPhishing = false;
+            deleteElements();
 
             let emailUrls = extractURLs(emailData.content_html);
             console.log("URLS DEL EMAIL", emailUrls);
